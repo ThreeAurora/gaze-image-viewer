7c9eefc03cf318d7e678543c9da3dfc0683342a3
Aurora <chengweida2010@163.com>
2026-08-26 15:38:20 +0800

    更新(共享头:filegrid): 调整 filegrid.h

--- src/filegrid.h
+++ src/filegrid.h
@@ -148,7 +148,6 @@ private:
     QHash<QString, QPixmap> m_thumbCache;
 
     QTimer m_resizeTimer;
-    QTimer m_scrollSelTimer;   // 滚动停止后联动选中(防抖)
     bool   m_loading = false;
 
     static constexpr int SPACING = 6;
