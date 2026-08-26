c588f1f90a2df4933ed8fed810a264f2662da7f3
Aurora <chengweida2010@163.com>
2026-08-27 06:21:06 +0800

    更新(共享头:previewpanel): 调整 previewpanel.h

--- src/previewpanel.h
+++ src/previewpanel.h
@@ -42,6 +42,7 @@ protected:
 
 private:
     void setupPlayer();
+    void ensureVideoWidget();   // 视频控件唯一创建/复用入口
     void showImage(const QString &path);
     void showVideo(const QString &path);
     void showAudio(const QString &path);
