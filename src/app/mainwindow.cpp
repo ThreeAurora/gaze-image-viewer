b5023b2aaca1cfbf97c62c339bbbcefc77faef6f
Aurora <chengweida2010@163.com>
2026-08-28 07:31:09 +0800

    更新(主窗口): mainwindow.cpp：动到 MainWindow::selftestFastScroll

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -366,6 +366,7 @@ void MainWindow::selftestFastScroll() {
     Logger::boot("ctor:preview");
 
     createStatusbar();
+    applyPaneVisibility();
 
     // 最近文件合批写盘:连续切换只刷内存,静默 500ms 后一次落盘
     m_recentFlushTimer.setSingleShot(true);
