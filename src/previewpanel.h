f38b2f33b070bc75850aa2877fce59c8567ce0ac
Aurora <chengweida2010@163.com>
2026-08-27 10:21:41 +0800

    更新(共享头:previewpanel): 调整 previewpanel.h

--- src/previewpanel.h
+++ src/previewpanel.h
@@ -28,8 +28,8 @@ public:
     void togglePlayPause();
     void seekDelta(int seconds);
 
-    int scalePercent() const { return int(m_scale * 100); }
-    bool hasPixmap() const { return m_origPix != nullptr; }
+signals:
+    void navFile(int delta);
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
