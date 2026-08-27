e73bfa159c8dd7e14d3c20100f03d62745fc2854
Aurora <chengweida2010@163.com>
2026-08-28 04:29:47 +0800

    更新(共享头:previewpanel): 调整 previewpanel.h

--- src/previewpanel.h
+++ src/previewpanel.h
@@ -38,6 +38,7 @@ signals:
 protected:
     void resizeEvent(QResizeEvent *event) override;
     void paintEvent(QPaintEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
     void contextMenuEvent(QContextMenuEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
