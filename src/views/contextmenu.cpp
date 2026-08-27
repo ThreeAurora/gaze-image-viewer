16a011a73d49ac65ff5548dd4a6ddf7fb3bc7a71
Aurora <chengweida2010@163.com>
2026-08-28 03:27:17 +0800

    更新(右键菜单): contextmenu.cpp：动到 FileContextMenu::FileContextMenu

--- src/views/contextmenu.cpp
+++ src/views/contextmenu.cpp
@@ -164,8 +164,6 @@ FileContextMenu::FileContextMenu(FileCard* card, QWidget* parent)
     });
     addAction(IconLib::appIcon("cmd_delete"), "删除", this, [sel, grid, this]() {
         if (!deleteWithSettings(sel, this)) return;
-        if (!deleteToRecycleBin({}))
-            QMessageBox::warning(nullptr, "删除", "删除到回收站失败");
         if (grid) grid->refreshCurrentDir();
     });
     addAction(IconLib::appIcon("cmd_rename"), "重命名...", this, [this, grid]() {
