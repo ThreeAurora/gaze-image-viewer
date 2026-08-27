edae0f9c09fb1ee3d990193d207690dfd00835dc
Aurora <chengweida2010@163.com>
2026-08-28 00:01:08 +0800

    更新(共享头:fileentry): fileentry.h：动到 imageSize

--- src/fileentry.h
+++ src/fileentry.h
@@ -178,26 +178,6 @@ inline QSize imageSize(const QString& filePath) {
 #include <shobjidl.h>
 #include <commoncontrols.h>  // IImageList 接口 + IID_IImageList(GUID 声明,uuid 库给定义)
 
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-// 系统图标提取需要 Vista+ API(SHGetImageList/SHIL_JUMBO 256px)
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
-#endif
-#ifndef NTDDI_VERSION
-#define NTDDI_VERSION 0x06000000
-#endif
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x0600
-#endif
-#include <windows.h>
-#include <shellapi.h>
-#include <shlobj.h>
-#include <commctrl.h>        // ILD_TRANSPARENT
-#include <shobjidl.h>
-#include <commoncontrols.h>  // IImageList 接口 + IID_IImageList(GUID 声明,uuid 库给定义)
-
 // HICON → QImage(Qt6 无 QPixmap::fromHICON,用 GetDIBits 手工转换)
 inline QImage hiconToQImage(HICON hIcon) {
     ICONINFO ii = {};
