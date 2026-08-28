74dc66ac2a0479cf344c5fccba76eadf3e8d8fda
Aurora <chengweida2010@163.com>
2026-08-29 06:03:11 +0800

    更新(共享头:mainwindow): 调整 mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -87,6 +87,7 @@ private:
     void createLayoutMenu();                   // 菜单栏"布局"(追加到当前末尾)
     void applyLastLayout();                  // 应用上次关闭时的状态
     QString splitterCsv() const;             // 可落盘的分栏宽度(查看器模式下取进入前的值)
+    QString splitterCsv() const;             // 可落盘的分栏宽度(查看器模式下取进入前的值)
 
     // 批次 2:菜单/工具栏/标记/最近文件
     QMenu* createViewModeMenu(QWidget* parent);   // 查看方式 7 种
