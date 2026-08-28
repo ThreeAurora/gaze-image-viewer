a74545247977e054ebfb92573ba71a581a840f72
Aurora <chengweida2010@163.com>
2026-08-28 18:35:19 +0800

    更新(预览查看器): previewpanel.cpp：动到 PreviewPanel::PreviewPanel

--- src/views/previewpanel.cpp
+++ src/views/previewpanel.cpp
@@ -293,7 +293,7 @@ PreviewPanel::PreviewPanel(QWidget* parent) : QWidget(parent) {
         add(QStyle::SP_DialogResetButton, QString::fromUtf8("适应窗口"),
             [this]() { fitAuto(); });
         add(QStyle::SP_FileDialogDetailedView, QString::fromUtf8("1:1"),
-            [this]() { if (m_origPix) { m_scale = 1.0; m_ctrlZoomed = true; render(); } });
+            [this]() { if (m_origPix) { m_scale = oneToOneScale(); m_ctrlZoomed = true; render(); } });
         add(QStyle::SP_DialogCloseButton, QString::fromUtf8("退出全屏"),
             [this]() { if (inFullscreen()) window()->showNormal(); });
     }
