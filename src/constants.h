ec8a138659af9c4e09afc82df746165188c245fb
Aurora <chengweida2010@163.com>
2026-08-28 13:10:34 +0800

    更新(共享头:constants): constants.h：动到 {

--- src/constants.h
+++ src/constants.h
@@ -50,3 +50,13 @@ inline const std::unordered_set<QString> VIDEO_EXTS = {
 inline const std::unordered_set<QString> AUDIO_EXTS = {
     ".mp3", ".wav", ".flac", ".aac", ".ogg", ".wma", ".m4a", ".opus"
 };
+
+inline const std::unordered_set<QString> DOCUMENT_EXTS = {
+    ".txt", ".doc", ".docx", ".pdf", ".rtf", ".odt", ".xls", ".xlsx",
+    ".ppt", ".pptx", ".csv", ".md", ".tex", ".epub", ".mobi"
+};
+
+inline const std::unordered_set<QString> EXECUTABLE_EXTS = {
+    ".exe", ".bat", ".cmd", ".ps1", ".sh", ".msi", ".com", ".scr",
+    ".jar", ".py", ".pl", ".rb"
+};
