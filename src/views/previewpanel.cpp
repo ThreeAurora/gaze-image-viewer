c3ca94ae563e5e04f002d668fec83225992c47c9
Aurora <chengweida2010@163.com>
2026-08-28 22:31:12 +0800

    更新(预览查看器): previewpanel.cpp：动到 PreviewPanel::teardownPlayer

--- src/views/previewpanel.cpp
+++ src/views/previewpanel.cpp
@@ -1135,6 +1135,7 @@ void PreviewPanel::setupPlayer() {
 
 void PreviewPanel::teardownPlayer() {
     if (!m_player) return;
+    Logger::event(QStringLiteral("teardownPlayer src='%1'").arg(m_player->source().toLocalFile()));
     // 立即断开视频输出:阻止播放器继续往 QVideoWidget 渲染帧
     if (m_vw)
         m_player->setVideoOutput(static_cast<QVideoWidget*>(nullptr));
