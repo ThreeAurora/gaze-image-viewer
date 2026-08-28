3b03f88e375d3f4a1b64d12f3e28cbd568bdf9fa
Aurora <chengweida2010@163.com>
2026-08-28 10:36:05 +0800

    更新(共享头:filegrid): 调整 filegrid.h

--- src/filegrid.h
+++ src/filegrid.h
@@ -185,6 +185,7 @@ private:
     QSet<QString>        m_marked;
     QString              m_preferPath;  // 一次性:本次 loadDirectory 完成后要选中的路径
     QString              m_preferPath;  // 一次性:本次 loadDirectory 完成后要选中的路径
+    QString              m_preferPath;  // 一次性:本次 loadDirectory 完成后要选中的路径
 
     // 对象池
     std::vector<FileCard*> m_pool;
