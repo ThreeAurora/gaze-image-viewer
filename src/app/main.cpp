e4e58062c565dbbf812a0612da188502a07d0583
Aurora <chengweida2010@163.com>
2026-08-28 20:12:12 +0800

    更新(程序入口): 调整 main.cpp

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,6 @@
 #include <QApplication>
 #include <QIcon>
 #include <QFileInfo>
-#include <QFile>
-#include <QDateTime>
-#include <QDebug>
 #include <QDir>
 #include <QLocalServer>
 #include <QLocalSocket>
