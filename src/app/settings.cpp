ec4025997bfc2a831e387065e8c75972b9f9bf3a
Aurora <chengweida2010@163.com>
2026-08-29 15:39:05 +0800

    更新(设置存取): settings.cpp：动到 AppSettings::set

--- src/app/settings.cpp
+++ src/app/settings.cpp
@@ -52,48 +52,6 @@ void AppSettings::set(const QString& key, const QVariant& v) {
     emit changed();
 }
 
-bool AppSettings::livePhotoAutoPlay() const {
-    return m_settings.value("livephoto/autoplay", true).toBool();
-}
-void AppSettings::setLivePhotoAutoPlay(bool on) {
-    m_settings.setValue("livephoto/autoplay", on);
-}
-
-int AppSettings::thumbnailSize() const {
-    return m_settings.value("ui/thumbnail_size", 160).toInt();
-}
-void AppSettings::setThumbnailSize(int size) {
-    m_settings.setValue("ui/thumbnail_size", size);
-}
-
-int AppSettings::startupMode() const {
-    return m_settings.value("startup/mode", 0).toInt();
-}
-void AppSettings::setStartupMode(int mode) {
-    m_settings.setValue("startup/mode", mode);
-}
-
-QString AppSettings::startupPath() const {
-    return m_settings.value("startup/path", "").toString();
-}
-void AppSettings::setStartupPath(const QString& path) {
-    m_settings.setValue("startup/path", path);
-}
-
-int AppSettings::theme() const {
-    return m_settings.value("ui/theme", 0).toInt();
-}
-void AppSettings::setTheme(int t) {
-    m_settings.setValue("ui/theme", t);
-}
-
-bool AppSettings::videoAutoPlay() const {
-    return m_settings.value("video/autoplay", false).toBool();
-}
-void AppSettings::setVideoAutoPlay(bool on) {
-    m_settings.setValue("video/autoplay", on);
-}
-
 QVariant AppSettings::get(const QString& key, const QVariant& def) const {
     return m_settings.value(key, def);
 }
