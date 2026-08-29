02548f99be72cdfc966766ef48a1bbdbffe7caa4
Aurora <chengweida2010@163.com>
2026-08-30 05:11:15 +0800

    更新(程序入口): main.cpp：动到 main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -274,6 +274,7 @@ int main(int argc, char *argv[]) {
 
     Logger::boot("qss");
     Logger::boot("qss");
+    Logger::boot("qss");
     MainWindow w;
     Logger::boot("mw-ctor");
     Logger::boot("mw-ctor");
