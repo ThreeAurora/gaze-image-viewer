68f22bb4019426dfb38f7376db51aa7919867e0a
Aurora <chengweida2010@163.com>
2026-08-30 01:38:28 +0800

    更新(主窗口): mainwindow.cpp：动到 MainWindow::MainWindow

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -281,7 +281,9 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     tv->addWidget(createPaneHeader(QString::fromUtf8("文件夹"), "tree"));
     Logger::boot("tree:header");
 
+    Logger::boot("tree:before-ft");
     m_folderTree = new FolderTree;
+    Logger::boot("tree:after-ft");
     m_folderTree->setMinimumWidth(160);
     connect(m_folderTree, &FolderTree::folderSelected, this, &MainWindow::navigateTo);
     tv->addWidget(m_folderTree, 1);
