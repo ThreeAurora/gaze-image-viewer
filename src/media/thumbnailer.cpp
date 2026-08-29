69e5e012c95ccd95eb4c3f88019c439dc86cd045
Aurora <chengweida2010@163.com>
2026-08-30 00:23:51 +0800

    更新(缩略图管线): 调整 thumbnailer.cpp

--- src/media/thumbnailer.cpp
+++ src/media/thumbnailer.cpp
@@ -23,6 +23,7 @@
 #include <QCryptographicHash>
 #include <QPixmap>
 #include <QImageReader>
+#include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 
 #include <windows.h>
 #include <shobjidl.h>
