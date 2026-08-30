6c3b9c6073a507f77a28fcc12f88c8b78356c08c
Aurora <chengweida2010@163.com>
2026-08-30 18:37:20 +0800

    更新(预览查看器): previewpanel.cpp：动到 PreviewPanel::showText

--- src/views/previewpanel.cpp
+++ src/views/previewpanel.cpp
@@ -1186,6 +1186,7 @@ void PreviewPanel::showText(const QString& path) {
     m_videoWidget->hide();
     m_audioLabel->hide();
     m_controlBar->hide();
+    m_imgSpace->hide();
     if (m_liveBadge) m_liveBadge->hide();
 
     QFile f(path);
