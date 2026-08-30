8ea8ab3df35df6e3cb63edb4e490ff7426c1810a
Aurora <chengweida2010@163.com>
2026-08-30 11:49:30 +0800

    更新(缩略图管线): 调整 thumbnailer.cpp

--- src/media/thumbnailer.cpp
+++ src/media/thumbnailer.cpp
@@ -25,6 +25,7 @@
 #include <QImageReader>
 #include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 #include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
+#include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 
 #include <windows.h>
 #include <shobjidl.h>
