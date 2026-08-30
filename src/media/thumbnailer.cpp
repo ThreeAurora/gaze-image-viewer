3d824e3eac0b8d028f2037aa6d2d2ba99a377ff3
Aurora <chengweida2010@163.com>
2026-08-30 13:42:14 +0800

    更新(缩略图管线): 调整 thumbnailer.cpp

--- src/media/thumbnailer.cpp
+++ src/media/thumbnailer.cpp
@@ -26,6 +26,7 @@
 #include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 #include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 #include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
+#include <algorithm>   // 必须在 windows.h 之前:min/max 宏会咬坏 libstdc++ 头
 
 #include <windows.h>
 #include <shobjidl.h>
