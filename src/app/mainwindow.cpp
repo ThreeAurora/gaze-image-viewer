c7682971632fc641ad736ef7a1322c9624d973d9
Aurora <chengweida2010@163.com>
2026-08-31 14:35:39 +0800

    更新(主窗口): mainwindow.cpp：动到 MainWindow::MainWindow

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -275,7 +275,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     ml->addWidget(m_viewerTabs);
 
     m_splitter = new QSplitter(Qt::Horizontal);
-    m_splitter->setStyleSheet("QSplitter::handle{background:" C_SEPARATOR ";width:1px;}");
+    m_splitter->setStyleSheet(QString::fromUtf8(
+        "QSplitter::handle{background:%1;width:1px;}").arg(C_SEPARATOR));
     ml->addWidget(m_splitter, 1);
 
     // 树面板:"文件夹"标题条 + FolderTree(标题条右侧 X 关闭)
