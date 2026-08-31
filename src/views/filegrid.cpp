96acc9f948b3a65d1118fbf22bbd06144370332d
Aurora <chengweida2010@163.com>
2026-08-31 19:43:58 +0800

    更新(文件列表): filegrid.cpp：动到 FileGrid::FileGrid

--- src/views/filegrid.cpp
+++ src/views/filegrid.cpp
@@ -113,7 +113,7 @@ FileGrid::FileGrid(QWidget* parent) : QScrollArea(parent) {
     m_viewMode  = qBound(0, AppSettings::instance().get("Browser/viewMode", int(VM_THUMBS_NAME)).toInt(), int(VM_WATERFALL));
     m_nameOrder = qBound(0, AppSettings::instance().get("Browser/nameOrder", int(NameNatural)).toInt(), int(NameNormal));
     // #106:筛选模式同样落盘恢复(此前切"视频"重启回"全部")
-    m_filterMode = qBound(int(FILTER_ALL), AppSettings::instance().get("Browser/filterMode", int(FILTER_ALL)).toInt(), int(FILTER_UNRED));
+    m_filterMode = qBound(int(FILTER_ALL), AppSettings::instance().get("Browser/filterMode", int(FILTER_ALL)).toInt(), int(FILTER_CUSTOM));
     // #106:筛选模式同样落盘恢复(此前切"视频"重启回"全部")
     m_filterMode = qBound(int(FILTER_ALL), AppSettings::instance().get("Browser/filterMode", int(FILTER_ALL)).toInt(), int(FILTER_UNRED));
     m_spacing   = qBound(0, AppSettings::instance().get("Appearance/spacing", 6).toInt(), 40);
