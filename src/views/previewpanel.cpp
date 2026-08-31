25c6bc448821e493b7217e93dff612f7429ddec7
Aurora <chengweida2010@163.com>
2026-09-01 03:33:44 +0800

    更新(预览查看器): previewpanel.cpp：动到 PreviewPanel::PreviewPanel

--- src/views/previewpanel.cpp
+++ src/views/previewpanel.cpp
@@ -103,6 +103,16 @@ PreviewPanel::PreviewPanel(QWidget* parent) : QWidget(parent) {
     m_audioLabel->hide();
     layout->addWidget(m_audioLabel, 1);
 
+    // 音频波形画布:解码+聚合在专属线程(audiowave.h),这里只收快照画像素。
+    // stretch 3:波形吃音频区大头,文件名条占 1/4
+    m_waveLabel = new QLabel;
+    m_waveLabel->setAlignment(Qt::AlignCenter);
+    m_waveLabel->setStyleSheet(
+        QString("color:%1;font-size:12px;background:transparent;").arg(C_TEXT_DIM));
+    m_waveLabel->hide();
+    m_waveLabel->installEventFilter(this);   // Resize → renderWave 重画
+    layout->addWidget(m_waveLabel, 3);
+
     // txt 文本预览(等宽字体,只读,深色底)
     m_textEdit = new QTextEdit;
     m_textEdit->setReadOnly(true);
