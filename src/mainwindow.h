4a466f3f768c8ab9a498969173d89480c3eb7c9a
Aurora <chengweida2010@163.com>
2026-09-01 10:33:37 +0800

    更新(共享头:mainwindow): 调整 mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -90,6 +90,7 @@ private:
     void onThumbZoom(int delta);
     void goBack();
     void goForward();
+    void goUp();
     void gotoTypedPath();   // 地址栏回车(#109②):目录进目录 / 文件定位到它 / 都不像就吭一声
     void gotoTypedPath();   // 地址栏回车(#109②):目录进目录 / 文件定位到它 / 都不像就吭一声
     void gotoTypedPath();   // 地址栏回车(#109②):目录进目录 / 文件定位到它 / 都不像就吭一声
