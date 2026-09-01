6ad73e2dfd30e9f2d6e5d6ae717919ac3db1dfb2
Aurora <chengweida2010@163.com>
2026-09-01 08:43:16 +0800

    更新(主窗口): mainwindow.cpp：动到 MainWindow::MainWindow

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -428,11 +428,13 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     iv->setContentsMargins(0, 0, 0, 0);
     iv->setSpacing(0);
     iv->addWidget(createPaneHeader(QString::fromUtf8("信息"), "info"));
+    Logger::boot("ctor:info-pre");
     m_info = new InfoPanel;
     m_info->setMinimumHeight(140);
     iv->addWidget(m_info, 1);
     m_infoPane->setMinimumHeight(140);
     pv->addWidget(m_infoPane);
+    Logger::boot("ctor:info-post");
     connect(m_info, &QWidget::destroyed, this, [this]() { m_info = nullptr; });
 
     m_splitter->addWidget(previewPane);
