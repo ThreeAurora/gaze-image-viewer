3c405d5c0c6db21033a3fbf988028af568d65eca
Aurora <chengweida2010@163.com>
2026-09-01 17:52:36 +0800

    更新(设置存取): 调整 settings.cpp

--- src/app/settings.cpp
+++ src/app/settings.cpp
@@ -1,8 +1,10 @@
 #include "settings.h"
+#include "logger.h"
 #include <QCoreApplication>
 #include <QStandardPaths>
 #include <QDir>
 #include <QFileInfo>
+#include <QElapsedTimer>
 #include <QFile>
 #include <QFile>
 
